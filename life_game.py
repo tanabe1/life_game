d1fd481ea11874d7d5d320507511c7c083fc7331
tanabe <you@example.com>
2020-08-21 19:36:31 -0700

    時間測定追加

--- life_game.py
+++ life_game.py
@@ -3,15 +3,17 @@
 import numpy
 import curses
 from curses import wrapper
+import time
 
 row2str = lambda row: "".join(["0" if c != 0 else " " for c in row])
 cell_value = lambda world, height, width, y, x : world[y % height, x % width]
 
-def print_world(stdscr, world):
+def print_world(stdscr, world, generation, elapsed):
     height, width = world.shape
     for y in range(height):
         row = world[y]
         stdscr.addstr(y, 0, row2str(row))
+    stdscr.addstr(height, 0, "generation %06d, Elapsed %.6f[sec]" %(generation, elapsed/generation)) 
     stdscr.refresh()
 
 def set_next_cell_value(world, next_world, height, width, y, x):
@@ -49,9 +51,16 @@ def life_game(stdscr, height, width):
 
     # 次の世代の世界を保持する２次元配列
     next_world = numpy.empty((height, width), dtype=numpy.int32)
+
+    elapsed = 0.0
+    generation = 0
     while True:
-        print_world(stdscr, world)
+        generation += 1
+        print_world(stdscr, world, generation, elapsed)
+        start_time = time.time()
         calc_next_world(world, next_world)
+        duration = time.time() - start_time
+        elapsed += duration
         world, next_world = next_world, world
 
 
